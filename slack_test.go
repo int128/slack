868cef62b9123f9df08466b22a55b755ef609a01
Hidetake Iwata <int128@gmail.com>
2018-10-30 10:05:25 +0900

    Add example of Client.Send()

--- slack_test.go
+++ slack_test.go
@@ -2,6 +2,7 @@ package slack_test
 
 import (
 	"fmt"
+
 	"github.com/int128/slack"
 )
 
@@ -9,6 +10,20 @@ const webhook = "https://hooks.slack.com/services/..."
 
 func ExampleSend() {
 	if err := slack.Send(webhook, &slack.Message{
+		Username:  "mybot",
+		IconEmoji: ":star:",
+		Text:      "Hello World!",
+	}); err != nil {
+		panic(fmt.Errorf("Could not send the message to Slack: %s", err))
+	}
+}
+
+func ExampleClient_Send() {
+	c := &slack.Client{
+		WebhookURL: webhook,
+		HTTPClient: nil, // urlfetch.Client(ctx) on App Engine
+	}
+	if err := c.Send(&slack.Message{
 		Username:  "mybot",
 		IconEmoji: ":star:",
 		Text:      "Hello World!",
